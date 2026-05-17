afd70dddf3507ac2deba7013b5eec3096c05e4fb
Chandika S <97661137+chandika-s@users.noreply.github.com>
2026-05-17 17:07:43 +0530

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,9 +2,9 @@
 
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-dpp-2026-ap-south-1"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-dpp-2026"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking
   }
